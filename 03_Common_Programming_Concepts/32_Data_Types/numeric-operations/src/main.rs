8dda4fce1c705b780ca4825388afd29e35e3ec28
solareenlo <solareenlo@gmail.com>
2018-12-14 11:26:41 +0900

    四則演算と余りも一般的な使い方

---
+++ 03_Common_Programming_Concepts/32_Data_Types/numeric-operations/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let sum = 5 + 10;
+    let difference = 90.323 - 23423.34;
+    let product = 4 * 32;
+    let quotient = 4.45 / 7.3;
+    let remainder = 43 % 5;
+    println!("sum = {}", sum);
+    println!("difference = {}", difference);
+    println!("product = {}", product);
+    println!("quotient = {}", quotient);
+    println!("remainder = {}", remainder);
+/*
+    sum = 15
+    difference = -23333.017
+    product = 128
+    quotient = 0.6095890410958904
+    remainder = 3
+    と表示される.
+*/
+}
